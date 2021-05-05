56979328e52b27c9bede5c7d4dc4332bcfe30efe
Maddy <maddison.ippolito@gmail.com>
2021-05-05 22:23:43 +1000

    extract tickets  to only contain some propeprties

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+function getTickets(){
+
 const https = require('https')
+const _ = require('lodash')
 
 const options = {
     hostname: 'maddythedietitian.zendesk.com', 
@@ -8,9 +11,7 @@ const options = {
     'Content-Type': 'application/json',
     'Authorization': 'Basic bWFkZGlzb24uaXBwb2xpdG9AZ21haWwuY29tOkFnZW9ma2luZ3M3Jg=='
     },
-    
 }
-
 const req = https.request(options,(res) => {
     let body = "";
 console.log(res);
@@ -19,7 +20,11 @@ res.on('data', (chunk) => {
   });
   res.on('end', () => {
     let json = JSON.parse(body);
-    console.log(json);
+    json = json.tickets;
+    console.log(json);     
+    var cleanedTickets = extract(json);
+    console.log(cleanedTickets); 
+
     console.log('No more data in response.');
   });
 });
@@ -28,3 +33,23 @@ req.on('error', (e) => {
 console.error(`problem with request: ${e.message}`);
   });
 req.end()
+
+ function extract (json) {
+    let picked = []; 
+    for (var i = 0; i < json.length; i++){
+    let subset = _.pick(json[i], ['subject', 'created_at', 'type']);
+    picked.push(subset);
+    } 
+    return picked;
+ }
+
+ app.get('/main', function(req, res) {
+
+    var name = 'hello';
+  
+    res.render(__dirname + "/views/layouts/main.html", {name:name});
+  
+  });
+return picked; 
+
+}
